b447df07b3f31589e10d7f9e3b63d40b6793ef88
codepoet80 <jonathan.m.wise@gmail.com>
2021-12-18 11:42:53 -0500

    try to match palm api

--- getlocation-byip.php
+++ getlocation-byip.php
@@ -10,18 +10,32 @@ $config = include('config.php');
 $ipinfoKey = $config['ipinfoKey'];
 $client_key = $config['clientids'];
 
+$data = (object) [
+	'returnValue' => true,
+	'altitude' => -1,
+	'heading' => -1,
+	'horizAccuracy' => -1,
+	'latitude' => -1,
+	'longitude' => -1,
+	'timestamp' => round(microtime(true) * 1000),
+	'velocity' => -1,
+	'vertAccuracy' => -1,
+	'errorCode' => 0,
+	'responseText' => ''
+];
+
 $request_headers = get_request_headers();
 if ($client_key != '') {	//If configuration includes both client key values, enforce them
 	if (!array_key_exists('Client-Id', $request_headers)) {
-			echo "{\"status\": \"error\", \"msg\": \"ERROR: Not authorized\"}";
-			die;
+		$data->errorCode = 8;
+		$data->responseText = "No Client-Id in request header";
+		print_r (json_encode($data));
 	} else {
 			$request_key = $request_headers['Client-Id'];
 			if (!in_array($request_key, $client_key)) {
-					echo "{\"status\": \"error\", \"msg\": \"ERROR: No authorized user.\"}";
-					echo $request_key;
-					echo $client_key;
-					die;
+				$data->errorCode = 6;
+				$data->responseText = "Client-Id in request was not known";
+				print_r (json_encode($data));		
 			}
 	}
 }
@@ -38,14 +52,15 @@ $myfile = fopen($search_path, "rb");
 $content = stream_get_contents($myfile);
 fclose($myfile);
 if (!isset($useLoc) || $useLoc == "") {
-	echo "{\"status\": \"error\", \"msg\": \"ERROR: No usable response from Geolocation service. Query may have been malformed, or API quota may have been exceeded.\"}";
-	die;
+	$data->errorCode = 2;
+	$data->responseText = "Upstream location resolver response was empty";
+	print_r (json_encode($data));
 }
 
-//Return results
-$data = (object) [
-	'location' => (string) $useLoc,
-  ];
+$locationparts = explode(",", (string)$useLoc);
+$data->latitude = $locationparts[0];
+$data->longitude = end($locationparts);
+
 print_r (json_encode($data));
 
 ?>
